a574799d719fc22cbaa900047b5f75332fb28358
26nicolas1a <nicolas.amaral.ferreira@escola.pr.gov.br>
2026-09-21 11:51:13 -0300

    21/09/2026

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
-    const botoes = document.querySelectorAll("button");
+const botoes = document.querySelectorAll("button");
 
-    botoes.forEach(function (botao) {
-        let curtio = false; 
-        botao.addEventListener("click", botaoClicado);
-        function botaoClicado() {
-            console.log("fui clicado");
-            let texto = botao.querySelector("span");
-            if (curtiu === false);{
-                texto.textContent++;
-                curtiu = true;
-            } else{
-                texto.textContent--;
-                curtiu = false;
-            }
+botoes.forEach(function (botao) {
+    let curtiu = false; 
+
+    botao.addEventListener("click", function botaoClicado() {
+        console.log("fui clicado");
+        let texto = botao.querySelector("span");
+        let contadorAtual = parseInt(texto.textContent);
+
+        if (curtiu === false) {
+            texto.textContent = contadorAtual + 1;
+            curtiu = true;
+        } else {
+            texto.textContent = contadorAtual - 1;
+            curtiu = false;
         }
-    })
-    
+    });
+});
